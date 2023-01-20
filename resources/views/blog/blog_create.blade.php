46566ad7f3f0700ac14e8201b318e8f1986eee65
Diyorbek Fazliddinov <diyorbekfazliddinoff@gmail.com>
2023-01-20 13:22:32 +0500

    Contact

--- resources/views/blog/blog_create.blade.php
+++ resources/views/blog/blog_create.blade.php
@@ -1,32 +1,7 @@
 @extends('layouts.app')
 
 
-@section('sidebar')
-    <ul class="nav-menu custom-scrollbar">
-        <li class="back-btn">
-            <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i>
-            </div>
-        </li>
-        <li class="sidebar-main-title">
-            <div>
-                <h6>General </h6>
-            </div>
-        </li>
-        <li class="dropdown"><a class="nav-link menu-title" href="{{route('home')}}"><i data-feather="home"></i><span>Dashboard</span></a>
 
-        </li>
-        <li class="dropdown"><a class="nav-link menu-title" href="{{route('contact.index')}}"><i
-                    data-feather="align-justify"></i><span>Contact</span></a>
-
-        </li>
-        <li class="dropdown"><a class="nav-link menu-title" href="{{route('blog.index')}}"><i
-                    data-feather="align-justify"></i><span>Blogs</span></a>
-
-        </li>
-
-
-    </ul>
-@endsection
 @section('content')
     <div class="page-body">
         <div class="container-fluid">
