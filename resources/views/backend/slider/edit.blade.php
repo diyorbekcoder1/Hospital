32cd9032767ab42fedb01e2c14ffe187bd50a445
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-02-13 00:01:05 +0500

    Commit

---
+++ resources/views/backend/slider/edit.blade.php
@@ -0,0 +1,12 @@
+@extends('backend.layouts.limitless')
+@section('content')
+    <section class="content-header">
+        <h1> Slider
+            <small> | Update Slider</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="{!! url(getLang(). '/admin/slider') !!}"><i class="fa fa-tint"></i> Slider</a></li>
+            <li class="active">Update Slider</li>
+        </ol>
+    </section>
+@stop
