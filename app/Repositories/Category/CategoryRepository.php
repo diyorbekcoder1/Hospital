6e8aa8c11dabc3f6f97c83d8d8233688caf10332
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-02-16 12:53:54 +0500

    Commmit

--- app/Repositories/Category/CategoryRepository.php
+++ app/Repositories/Category/CategoryRepository.php
@@ -3,70 +3,113 @@
 namespace App\Repositories\Category;
 
 use App\Models\Categories;
-use App\Repositories\RepositoryAbstract;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Validation\ValidationException;
-use stdClass;
 
-class CategoryRepository extends RepositoryAbstract{
+use Config;
+
+
+class CategoryRepository extends RepositoryAbstract implements CategoryInterface
+{
     protected $perPage;
     protected $category;
-
-    protected static $rules = [
+     protected static $rules = [
         'title' => 'required|min:3|unique:categories',
     ];
 
     public function __construct(Categories $category)
     {
         $this->category = $category;
-        $config = Config::get('cms');
+        $config = Config::get('fully');
         $this->perPage = $config['per_page'];
     }
 
+    /**
+     * @return mixed
+     */
     public function all()
     {
         return $this->category->where('lang', $this->getLang())->get();
     }
 
+    /**
+     * @param int  $page
+     * @param int  $limit
+     * @param bool $all
+     *
+     * @return mixed|\StdClass
+     */
     public function paginate($page = 1, $limit = 10, $all = false)
     {
-        $result = new StdClass();
+        $result = new \StdClass();
         $result->page = $page;
         $result->limit = $limit;
         $result->totalItems = 0;
         $result->items = array();
+
         $query = $this->category->orderBy('title');
+
         $categories = $query->skip($limit * ($page - 1))->take($limit)->where('lang', $this->getLang())->get();
+
         $result->totalItems = $this->totalCategories();
         $result->items = $categories->all();
+
         return $result;
     }
 
+    /**
+     * @return mixed
+     */
     public function lists()
     {
-        return $this->category->where('lang', $this->getLang())->pluck('name', 'id');
+        return $this->category->where('lang', $this->getLang())->lists('title', 'id');
     }
 
+    /**
+     * @param $id
+     *
+     * @return mixed
+     */
     public function find($id)
     {
         return $this->category->findOrFail($id);
     }
 
+    /**
+     * @param $slug
+     *
+     * @return mixed
+     */
     public function getArticlesBySlug($slug)
     {
         return $this->category->where('slug', $slug)->where('lang', $this->getLang())->first()->articles()->paginate($this->perPage);
     }
 
+    /**
+     * @param $attributes
+     *
+     * @return bool|mixed
+     *
+     * @throws \Fully\Exceptions\Validation\ValidationException
+     */
     public function create($attributes)
     {
         if ($this->isValid($attributes)) {
             $this->category->lang = $this->getLang();
             $this->category->fill($attributes)->save();
+
             return true;
         }
+
         throw new ValidationException('Category validation failed', $this->getErrors());
     }
 
+    /**
+     * @param $id
+     * @param $attributes
+     *
+     * @return bool|mixed
+     *
+     * @throws \Fully\Exceptions\Validation\ValidationException
+     */
     public function update($id, $attributes)
     {
         $this->category = $this->find($id);
@@ -81,6 +124,11 @@ class CategoryRepository extends RepositoryAbstract{
         throw new ValidationException('Category validation failed', $this->getErrors());
     }
 
+    /**
+     * @param $id
+     *
+     * @return mixed|void
+     */
     public function delete($id)
     {
         $this->category = $this->category->find($id);
@@ -88,6 +136,11 @@ class CategoryRepository extends RepositoryAbstract{
         $this->category->delete();
     }
 
+    /**
+     * Get total category count.
+     *
+     * @return mixed
+     */
     protected function totalCategories()
     {
         return $this->category->where('lang', $this->getLang())->count();
--- app/Repositories/RepositoryAbstract.php
+++ app/Repositories/RepositoryAbstract.php
@@ -2,28 +2,14 @@
 
 namespace App\Repositories;
 
-/**
- * Class RepositoryAbstract.
- *
- * @author Sefa Karagöz <karagozsefa@gmail.com>
- */
+
 abstract class RepositoryAbstract extends AbstractValidator
 {
-    /**
-     * Get language.
-     *
-     * @return mixed
-     */
     protected function getLang()
     {
         return getLang();
     }
 
-    /**
-     * @param $string
-     *
-     * @return mixed
-     */
     protected function slug($string)
     {
         return filter_var(str_replace(' ', '-', strtolower(trim($string))), FILTER_SANITIZE_URL);
